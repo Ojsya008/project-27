ee74a7cff42fb8936c433b2daa5c4986b21dba9a
Ojsya008 <74088466+Ojsya008@users.noreply.github.com>
2021-03-17 10:39:51 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,13 @@
+class bob extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("sprites/bob.png");
+    }
+  
+    display() {
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+      super.display();
+    }
+  }
+  
